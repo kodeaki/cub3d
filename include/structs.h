7f942b25a6db41c2cdfc6c9eab97feb904bd732e
Joonatan Tarvainen <jtarvain.hive@gmail.com>
2026-04-21 14:41:45 +0300

    split h files, added parsing/utils.c, Makefile edits for libft, commented out main.c parsing

---
+++ include/structs.h
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtarvain <jtarvain@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 14:56:02 by jtarvain          #+#    #+#             */
+/*   Updated: 2026/04/21 13:21:43 by jtarvain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+# include "typedef.h"
+# include <stdbool.h>
+
+struct s_parser
+{
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+	char	*c;
+	char	*f;
+};
+
+struct s_check
+{
+	int	no;
+	int	so;
+	int	we;
+	int	ea;
+	int	c;
+	int	f;
+};
+
+struct s_config
+{
+
+};
+
+struct s_rgb
+{
+	int		r;
+	int		g;
+	int		b;
+};
+
+struct s_map
+{
+	char	**arr;
+	int		height;
+	int		width;
+};
+
+struct s_player
+{
+	float		x;
+	float		y;
+	float		angle;
+	float		fov;
+	float		turn_speed;
+	int			move_speed;
+
+	bool		key_up;
+	bool		key_down;
+	bool		key_left;
+	bool		key_right;
+	bool		left_rotate;
+	bool		right_rotate;
+
+};
+
+struct s_game
+{
+	void		*mlx;
+	void		*window;
+	void		*image;
+
+	char		*data;
+	int			bits_per_pixel;
+	int			size_line;
+	int			endian;
+
+	t_map		map;
+	t_player	player;
+};
+
+#endif
---
+++ src/parsing/utils.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtarvain <jtarvain@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 19:24:28 by jtarvain          #+#    #+#             */
+/*   Updated: 2026/04/17 19:27:20 by jtarvain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	ft_exit(int	err, const char *msg)
+{
+	printf("Error.\n");
+	printf("%s\n", msg);
+	exit(err);
+}
+
+int		ft_close(int fd, int ret)
+{
+	close(fd);
+	return (ret);
+}
