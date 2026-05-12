af5d419211065d97ca8ad564b1e777f20ea1f489
t-pirinen <toimi.i.pirinen@gmail.com>
2026-05-12 16:55:43 +0300

    deleted unnecessary file and improved clear_image performance

--- src/raycasting/clear_image.c
+++ src/raycasting/clear_image.c
@@ -6,7 +6,7 @@
 /*   By: tpirinen <tpirinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 15:27:36 by tpirinen          #+#    #+#             */
-/*   Updated: 2026/03/31 15:34:47 by tpirinen         ###   ########.fr       */
+/*   Updated: 2026/05/12 16:54:10 by tpirinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,30 @@ static uint32_t	convert_rgb_to_hex(t_rgb *rgb)
 
 // Clears old frame by painting the top half with the ceiling color
 // and the bottom half with the floor color
-void	clear_image(t_game *game)
+void clear_image(t_game *game)
 {
-	int			y;
-	int			x;
 	uint32_t	ceiling;
 	uint32_t	floor;
+	uint32_t	*buf;
+	size_t		total;
+	size_t		i;
 
 	ceiling = convert_rgb_to_hex(&game->config.c);
 	floor = convert_rgb_to_hex(&game->config.f);
-	y = 0;
-	while (y < game->window_height / 2)
+	buf = (uint32_t *)game->data;
+	total = (size_t)game->window_width * (size_t)game->window_height;
+
+	i = 0;
+	while (i < total / 2)
 	{
-		x = 0;
-		while (x < game->window_width)
-			put_pixel(game, ceiling, x++, y);
-		y++;
+		buf[i] = ceiling;
+		i++;
 	}
-	while (y < game->window_height)
+	i = total / 2;
+	while (i < total)
 	{
-		x = 0;
-		while (x < game->window_width)
-			put_pixel(game, floor, x++, y);
-		y++;
+		buf[i] = floor;
+		i++;
 	}
 }
+
