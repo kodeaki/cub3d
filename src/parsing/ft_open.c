9b49c463258c004ec96a53237fd474a396dbc17a
t-pirinen <toimi.i.pirinen@gmail.com>
2026-06-30 13:10:05 +0300

    finishing touches

--- src/parsing/ft_open.c
+++ src/parsing/ft_open.c
@@ -73,10 +73,9 @@ int	process_line(t_game *game, char *line, int i)
 	if (!ft_strncmp(line, "NO ", 3) || !ft_strncmp(line, "SO ", 3)
 		|| !ft_strncmp(line, "EA ", 3) || !ft_strncmp(line, "WE ", 3)
 		|| !ft_strncmp(line, "F ", 2) || !ft_strncmp(line, "C ", 2))
-	{
-		add_check(game, line);
-		return (0);
-	}
+		return (add_check(game, line), 0);
+	else
+	
 	if (game->file.map_start && empty_row(line))
 		return (1);
 	if (empty_row(line))
